0806825f672ed119fc55bccf51922e33818d3b48
Blagoje Blagojevic <blagojeblagojevic562@gmail.com>
2023-10-06 18:45:48 +0200

    Update magnetic.c

--- magnetic.c
+++ magnetic.c
@@ -21,11 +21,12 @@
 #undef main                //275 183
 #define width 500
 #define height 500
-#define SEED   100
+#define SEED   500
 //#define T 0
 //#define J 100
 #define RED 0x00FF0000
 #define BLUE 0x000000FF
+size_t iter=0;
 float T,J;
 float DISTRIBUTION;
 float M_TOTAL = 0.0f;
@@ -40,15 +41,15 @@ float RandF(void){
 void RandSpins(uint32_t spins[height][width]){
 		srand(time(0));
 	  //srand(SEED);
-	for(size_t y = 1;y < height-1;y++){
-		for(size_t x = 1;x < width-1;x++){
-			if(RandF() > DISTRIBUTION){
+	for(size_t y = 0;y < height;y++){
+		for(size_t x = 0;x < width;x++){
+			if(RandF() < DISTRIBUTION){
 				spins[y][x]= RED;  //BLUE
-				M_TOTAL--;
+				M_TOTAL++;
 			}
 			else{
 				spins[y][x]= BLUE;  //RED
-				M_TOTAL++;
+				M_TOTAL--;
 			}
 		}
 	}
@@ -56,8 +57,8 @@ void RandSpins(uint32_t spins[height][width]){
 
 float CalculateFermiDirackDistribution(float E){
 	float k = 1.380649; //MOVED 10E-23 AS COPLING CONSTANT 
-	
-	return 1.0f/(expf((E)/(k*T))+1); 	
+	//return 1;
+	return 1.0f/(expf((E)/(k*(T+0.001)))+1); 	
 }
 
 void header(void)
@@ -69,6 +70,18 @@ void header(void)
 	printf("LEGENDS: RED: +SPIN BLUE -SPIN\n\n");
 
 }
+void LodingParametars(void)
+{
+	printf("\nT =  ");
+	scanf("%f",&T);
+	printf("\n\nJ IS DIVIDED BY 10^-23\nJ = ");
+	scanf("%f",&J);
+	printf("INPUT DISTRIBUTION OF SPINS(0 - 1) ");
+	scanf("%f",&DISTRIBUTION);
+	assert(DISTRIBUTION > 0.0f);
+	assert(DISTRIBUTION < 1.0f);
+}
+
 
 void UpdateSpins(uint32_t spins[height][width])
 {
@@ -145,40 +158,37 @@ void UpdateSpins(uint32_t spins[height][width])
 			chance = CalculateFermiDirackDistribution(E_TOTAL);
 			if((RandF() < chance)&&(E_TOTAL > 0)){spins[x][y] = RED; }
 			if((RandF() < chance)&&(E_TOTAL < 0)){spins[x][y] = BLUE;}
+
 		
 		}
 		count++;
 		if(count%10000==0)
 		{
+			
+			printf("\nT =  ");
+			scanf("%f",&T);
 			system("cls");
+			
 			header();
 			M_TOTAL=0;
 			for(size_t y = 1;y < height-1;y++){
 			for(size_t x = 1;x < width-1;x++){
 			if(spins[y][x]==BLUE){M_TOTAL--;}
 			else{M_TOTAL++;}
+			count = 0;
 		}
 	}
-			printf("M = %f x10^(-3)\n",M_TOTAL/(float)(width*height));
-			count = 0; 
+			
+			printf("M(%d) = %f x10^(-3)\n",(int)T,(M_TOTAL)/(float)(width*height));
+			
+			//count = 0; 
 		}
 		E = 0;
-		//M_TOTAL = 0;
+		M_TOTAL = 0;
 	}
 	//system("pause");
 }
 
-void LodingParametars(void)
-{
-	printf("\nT =  ");
-	scanf("%f",&T);
-	printf("\n\nJ IS DIVIDED BY 10^-23\nJ = ");
-	scanf("%f",&J);
-	printf("INPUT DISTRIBUTION OF SPINS(0 - 1) ");
-	scanf("%f",&DISTRIBUTION);
-	assert(DISTRIBUTION > 0.0f);
-	assert(DISTRIBUTION < 1.0f);
-}
 int main()
 {
 	header();
